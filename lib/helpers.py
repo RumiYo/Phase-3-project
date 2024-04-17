795cff4b38fbc6de77cf95dbdd4aaa739b54e4b1
RumiYo <yokoyamarumi@gmail.com>
2024-04-17 00:09:36 -0400

    Fix indent error and set menu1 function

--- lib/helpers.py
+++ lib/helpers.py
@@ -10,17 +10,30 @@ def exit_program():
 def login():
     print("Performing useful function#1.")
     name = input("Enter your name: ")
-    return True
-
-
+    password = input("Enter your password: ")
+    user = User.find_by_name(name)
+    if user:
+        if user.password == password:
+            return True
+        else: 
+            print("The password is wrong")
+    else: 
+        print("The user name does not exist")
 
 def signup():
+    User.create_table()
     print("Performing useful function#2.")
     name = input("Enter your name: ")
     birth_year = input("Enter your birth year: ")
     print(f"Gener list: {User.GENDERS}")
     gender = input("Enter your gender (Select a number from above): ")
     email = input("Enter your email address: ")
-    print(name, birth_year, gender, email)
+    password = input("Create password (minimum 8 charactors): ")
+    try: 
+        user = User.create(name, int(birth_year), int(gender), email, password)
+        print(f"{user.name} account is successfully created.")
+        return True
+    except Exception as exc:
+        print("Error creating a user account", exc)
 
 
--- lib/models/user.py
+++ lib/models/user.py
@@ -11,14 +11,15 @@ class User:
         4: "Prefer not to say"
     }
 
-    def __init__(self, name, birth_year, gender, email):
+    def __init__(self, name, birth_year, gender, email, password):
         self.id = id
         self.name = name
         self.birth_year = birth_year
         self.gender = gender
         self.email = email
+        self.password = password
 
-    def __repr___(self):
+    def __repr__(self):
         return (
             f"<User {self.name} (Gender: {self.gender}, Birth year: {self.birth_year}, Email address: {self.email})"
         )
@@ -40,10 +41,10 @@ class User:
 
     @birth_year.setter
     def birth_year(self, birth_year):
-        if isinstance(birth_year, int) and birth_year <= 2025 and birth_year >= 1930 and len(birth_year)==4:
+        if isinstance(birth_year, int) and 1930<= birth_year <= 2024:
             self._birth_year = birth_year
         else:
-            raise TypeError("Birth year must be a number and betwreen 1950 nand 2024")
+            raise TypeError ("Birth year must be a number and between 1930 and 2024")
 
     @property
     def gender(self):
@@ -51,10 +52,10 @@ class User:
 
     @gender.setter
     def gender(self, gender): 
-        if isinstance(gender, int) and gender >= 1 and gender <= 4 :
+        if isinstance(gender, int) and 1 <= gender <= 4 :
             self._gender = gender
         else:
-            raise TypeErrror("Select from the options")
+            raise TypeError ("Select your gender from the options")
 
     @property 
     def email(self):
@@ -65,7 +66,18 @@ class User:
         if isinstance(email, str) and "@" in email:
             self._email = email
         else:
-            TypeError("Please type email address")
+            raise TypeError("Please type email address")
+
+    @property 
+    def password(self):
+        return self._password 
+
+    @password.setter
+    def password(self, password):
+        if isinstance(password, str) and len(password)>= 8:
+            self._password = password
+        else:
+            raise TypeError("Password must be longer than 8 charactors")
 
 
     @classmethod
@@ -77,92 +89,94 @@ class User:
                 birth_year INTEGER,
                 gender INTEGER,
                 email TEXT,
+                password TEXT
             )
         """
         CURSOR.execute(sql)
         CONN.commit()
 
-        @classmethod
-        def drop_table(cls):
-            sql = """
-                DROP TABLE IF EXISTS users;
-            """
-            CURSOR.execute(sql)
-            CONN.commit()
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS users;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
 
-        def save(self):
-            sql = """
-                INSERT INTO users (name, birth_year, gender, email)
-                VALUES (?, ?, ?)
-            """
-            CURSOR.execute(sql, (self.name, self.birth_year, self.gender, self.email))
-            CONN.commit()
+    def save(self):
+        sql = """
+            INSERT INTO users (name, birth_year, gender, email,password)
+            VALUES (?, ?, ?, ?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.birth_year, self.gender, self.email, self.password))
+        CONN.commit()
 
-            self.id = CURSOR.lastrowid
-            type(self).all[self.id] = self
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
 
-        def update(self):
-            sql = """
-                UPDATE users
-                SET name = ?, birth_year = ?, gender = ?, email = ?
-                WHERE id = ?
-            """
-            CURSOR.execute(sql, (self.name, self.birth_year, self.gender, self.email))
-            CONN.commit()
+    def update(self):
+        sql = """
+            UPDATE users
+            SET name = ?, birth_year = ?, gender = ?, email = ?, password = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.birth_year, self.gender, self.email, self.password))
+        CONN.commit()
 
-        def delete(self):
-            sql = """
-                DELETE FROM users
-                WHERE id = ?
-            """
-            CURSOR.execute(sql, (self.id, ))
-            CONN.commit()
+    def delete(self):
+        sql = """
+            DELETE FROM users
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id, ))
+        CONN.commit()
 
-            del type(self).all[self.id]
-            seld.id = None
+        del type(self).all[self.id]
+        seld.id = None
 
-        @classmethod
-        def create(cls, name, birth_year, gender, email):
-            user = cls(name, birth_year, gender, email)
-            user.save()
-            return user
-        
-        @classmethod
-        def instance_from_db(cls, row):
-            user = cls.all.ger(row[0])
-            if user: 
-                user.name = row[1]
-                user.birth_year = row[2]
-                user.gender = row[3]
-                user.email = row[4]
-            else:
-                user = cls(row[1], row[2], row[3])
-                user.id = row[0]
-                cls.all[user.id] = user
-            return user
+    @classmethod
+    def create(cls, name, birth_year, gender, email, password):
+        user = cls(name, birth_year, gender, email, password)
+        user.save()
+        return user
+    
+    @classmethod
+    def instance_from_db(cls, row):
+        user = cls.all.get(row[0])
+        if user: 
+            user.name = row[1]
+            user.birth_year = row[2]
+            user.gender = row[3]
+            user.email = row[4]
+            user.password = row[5]
+        else:
+            user = cls(row[1], row[2], row[3], row[4], row[5])
+            user.id = row[0]
+            cls.all[user.id] = user
+        return user
 
-        @classmethod
-        def get_all(cls):
-            sql = """
-                SELECT * FROM users
-            """
-            rows = CURSOR.execute(sql).fetchall()
-            return [cls.instance_from_db(row) for row in rows]
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT * FROM users
+        """
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
 
-        @classmethod
-        def find_by_id(cls, id):
-            sql = """
-                SELECT  * FROM users
-                WHERE id = ?
-            """
-            row = CURSOR.execute(sql, (id, )).fetchone()
-            return cls.instance_from_db(row) if row else None
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT  * FROM users
+            WHERE id = ?
+        """
+        row = CURSOR.execute(sql, (id, )).fetchone()
+        return cls.instance_from_db(row) if row else None
 
-        @classmethod
-        def find_by_name(cls, name):
-            sql = """
-                SELECT  * FROM users
-                WHERE name = ?
-            """
-            row = CURSOR.execute(sql, (name, )).fetchone()
-            return cls.instance_from_db(row) if row else None
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT  * FROM users
+            WHERE name = ?
+        """
+        row = CURSOR.execute(sql, (name, )).fetchone()
+        return cls.instance_from_db(row) if row else None
